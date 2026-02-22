ed585ecddd73b8fb14d479656f3b102836497669
kaede-rei <kaerei86@gmail.com>
2026-02-22 09:53:33 +0800

    清除杂项

--- build/pr2_arm_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kaerei/ROS_Workspace/multi_arm/src/pr2_arm_controller
-# Build directory: /home/kaerei/ROS_Workspace/multi_arm/build/pr2_arm_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/pr2_arm_msgs_srvs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kaerei/ROS_Workspace/multi_arm/src/pr2_arm_msgs_srvs
-# Build directory: /home/kaerei/ROS_Workspace/multi_arm/build/pr2_arm_msgs_srvs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
